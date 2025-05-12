5721a741986b77d399ee677b4cffaf865616e3ce
shiinarikka <541665621@qq.com>
2025-05-13 02:43:41 +0800

    refactor 架构重构  C语言部分设计AST结构体，对应在cangjie部分设计CAST用于对接接口，AST实现仓颉框架下的语法树。translate实现转译工作

---
+++ c/AST.c
@@ -0,0 +1,7 @@
+#include"AST.h"
+#include<stdio.h>
+#include<stdlib.h>
+AstNode* getAst()
+{
+    
+}
---
+++ c/AST.h
@@ -0,0 +1,12 @@
+#ifndef CSTRUCT
+#define CSTRUCT
+
+typedef struct 
+{
+    
+}AstNode;
+
+
+__attribute__((visibility("default"))) 
+AstNode* getAst();
+#endif
