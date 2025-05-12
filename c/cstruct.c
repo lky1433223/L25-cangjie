978937ae460d113f8d5dcf8f9976a8c58df51614
shiinarikka <541665621@qq.com>
2025-05-12 23:35:54 +0800

    init

---
+++ c/cstruct.c
@@ -0,0 +1,9 @@
+#include"cstruct.h"
+Point3D buildPoint()
+{
+    Point3D a;
+    a.x = 10;
+    a.y = 2;
+    a.z = 3;
+    return a;
+}
---
+++ c/cstruct.h
@@ -0,0 +1,6 @@
+typedef struct {
+    long long x;
+    long long y;
+    long long z;
+} Point3D;
+Point3D buildPoint();
