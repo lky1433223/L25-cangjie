6cca77ca3f68dc10a4ca1f39edd3e9a05d233ea8
shiinarikka <541665621@qq.com>
2025-05-23 15:52:09 +0800

    test

--- kill.sh
+++ kill.sh
@@ -1,2 +1,4 @@
 pgrep -f "/L25/target/release/bin/main"
+pgrep -f "/L25/target/debug/bin/main"
 pgrep -f "/L25/target/release/bin/main" | xargs kill -9
+pgrep -f "/L25/target/debug/bin/main" | xargs kill -9
